04ad7666dff832c9c183c8224c5169deda4d3282
MantasEdine <youcefmantas3@gmail.com>
2026-07-19 21:32:04 +0100

    adding lazers

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
 var meteor_scene : PackedScene = load('res://scenes/meteor.tscn')
+var lazer_scene : PackedScene = load('res://scenes/lazer.tscn')
+
+
 func _on_meteor_timer_timeout() -> void:
 	var meteor = meteor_scene.instantiate()
 	$Meteors.add_child(meteor)
+
+
+func _on_player_lazer(pos) -> void:
+	var lazer = lazer_scene.instantiate()
+	$Lazers.add_child(lazer)
+	lazer.position = pos
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed : int = 500
+signal lazer(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +13,8 @@ func _process(delta: float) -> void:
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction  * speed 
 	move_and_slide()
+
+	if Input.is_action_just_pressed("shoot"):
+		lazer.emit($LazerStartingPosition.global_position)
+		
 	
