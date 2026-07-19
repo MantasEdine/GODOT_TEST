ffe0950579d4e890275ff51e59ccfbf9571a8694
MantasEdine <youcefmantas3@gmail.com>
2026-07-19 01:43:35 +0100

    Timer Improvment

--- scenes/meteor.gd
+++ scenes/meteor.gd
@@ -1,5 +1,7 @@
 extends Area2D
-
+var speed : int
+var rotation_speed : int
+var direction : float
 
 func _ready() -> void:
 	var rng := RandomNumberGenerator.new()
@@ -8,9 +10,14 @@ func _ready() -> void:
 	var random_x = rng.randi_range(0,width)
 	var random_y = rng.randi_range(-150,-50)
 	position = Vector2(random_x,random_y)
+	speed = rng.randi_range(300,500)
+	direction = rng.randf_range(-1,1)
+	rotation_speed = rng.randi_range(40,100)
+	
 	
 func _process(delta):
-	position += Vector2(0,1.0) * 400 * delta
+	position += Vector2(direction,1.0) * 400 * delta
+	rotation_degrees += rotation_speed * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
