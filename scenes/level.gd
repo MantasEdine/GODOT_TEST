367f47536333436121c06723d22d861bfbbb45d1
MantasEdine <youcefmantas3@gmail.com>
2026-07-20 01:07:50 +0100

    done with basic animation

--- scenes/level.gd
+++ scenes/level.gd
@@ -4,6 +4,26 @@ var meteor_scene : PackedScene = load('res://scenes/meteor.tscn')
 var lazer_scene : PackedScene = load('res://scenes/lazer.tscn')
 
 
+func _ready() -> void:
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	
+	for star in $Stars.get_children():
+		
+		var random_x = rng.randi_range(0,int(size.x))
+		var random_y = rng.randi_range(0,int(size.y))
+		star.position = Vector2(random_x,random_y)
+		
+		var random_scale = rng.randf_range(0.5,0.7)
+		star.scale = Vector2(random_scale,random_scale)
+		
+		star.speed_scale = rng.randf_range(0.6,1.4)
+		
+		
+		
+
+
+
 func _on_meteor_timer_timeout() -> void:
 	var meteor = meteor_scene.instantiate()
 	$Meteors.add_child(meteor)
